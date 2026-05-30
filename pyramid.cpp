6d7af95ce3dbdd54b1b3e4f1599417b311ce4e53
fanuel08 <fanuel08@example.com>
2026-05-30 23:42:23 +0300

    Add colored pyramid visualization

---
+++ pyramid.cpp
@@ -0,0 +1,65 @@
+#include <GL/glut.h>
+#include <math.h>
+
+void display(void) {
+    glClear(GL_COLOR_BUFFER_BIT);
+    glColor3f(1.0, 0.0, 0.0);
+    
+    glBegin(GL_TRIANGLES);
+    // Front face
+    glColor3f(1.0, 0.0, 0.0);
+    glVertex3f(0.0f, 0.5f, 0.0f);
+    glVertex3f(-0.5f, -0.5f, 0.5f);
+    glVertex3f(0.5f, -0.5f, 0.5f);
+    
+    // Right face
+    glColor3f(0.0, 1.0, 0.0);
+    glVertex3f(0.0f, 0.5f, 0.0f);
+    glVertex3f(0.5f, -0.5f, 0.5f);
+    glVertex3f(0.5f, -0.5f, -0.5f);
+    
+    // Back face
+    glColor3f(0.0, 0.0, 1.0);
+    glVertex3f(0.0f, 0.5f, 0.0f);
+    glVertex3f(0.5f, -0.5f, -0.5f);
+    glVertex3f(-0.5f, -0.5f, -0.5f);
+    
+    // Left face
+    glColor3f(1.0, 1.0, 0.0);
+    glVertex3f(0.0f, 0.5f, 0.0f);
+    glVertex3f(-0.5f, -0.5f, -0.5f);
+    glVertex3f(-0.5f, -0.5f, 0.5f);
+    
+    // Bottom face
+    glColor3f(1.0, 0.0, 1.0);
+    glVertex3f(-0.5f, -0.5f, 0.5f);
+    glVertex3f(0.5f, -0.5f, 0.5f);
+    glVertex3f(0.5f, -0.5f, -0.5f);
+    
+    glVertex3f(0.5f, -0.5f, -0.5f);
+    glVertex3f(-0.5f, -0.5f, -0.5f);
+    glVertex3f(-0.5f, -0.5f, 0.5f);
+    glEnd();
+    
+    glFlush();
+}
+
+void init(void) {
+    glClearColor(0.0, 0.0, 0.0, 1.0);
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    glOrtho(-1.0, 1.0, -1.0, 1.0, -1.0, 1.0);
+    glViewport(0, 0, 300, 200);
+    glMatrixMode(GL_MODELVIEW);
+}
+
+int main(int argc, char** argv) {
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
+    glutInitWindowSize(300, 200);
+    glutCreateWindow("Colored Pyramid");
+    init();
+    glutDisplayFunc(display);
+    glutMainLoop();
+    return 0;
+}
